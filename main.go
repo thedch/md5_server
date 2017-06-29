36df04b90ed44e29da93b48b3302d5b838ef67a3
Daniel Hunter <officialdanielhunter@gmail.com>
2017-06-29 11:00:01 -0700

    Added partial sql functionality to md5 app

--- main.go
+++ main.go
@@ -1,17 +1,70 @@
 package main
 
 import (
-  "fmt"
-  "github.com/thedch/md5_server/sums"
-  "net/http"
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	_ "github.com/lib/pq"
+	"github.com/thedch/md5_server/sums"
+)
+
+// Reading files requires checking most calls for errors.
+// This helper will streamline the error checks below.
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "postgres"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello there! The md5 hash of %s is %x", r.URL.Path[1:],
-      sums.GetMD5Hash(r.URL.Path[1:]))
+	input := r.URL.Path[1:]
+	// Calculate the hash
+	hash := sums.GetMD5Hash(r.URL.Path[1:])
+	// Print it for the user
+	fmt.Fprintf(w, "Hello there! The md5 hash of %s is %x\n", input, hash)
+
+	// Open the database, check for errors
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	check(err)
+	defer db.Close()
+
+	// Ping the database to ensure an actual connection
+	err = db.Ping()
+	check(err)
+
+	// Add the user input to the database
+	sqlInsert := `
+    INSERT INTO md5 (input, hash)
+    VALUES ($1, 'test')`
+
+	_, err = db.Exec(sqlInsert, input) // Underscore?
+	check(err)
+
+	// Query the database for infomation
+	sqlStatement := "SELECT input FROM md5;"
+	rows, err := db.Query(sqlStatement)
+	check(err)
+	defer rows.Close()
+	for rows.Next() {
+		var hash string
+		err = rows.Scan(&hash)
+		check(err)
+		fmt.Fprintln(w, hash)
+	}
+	err = rows.Err()
+	check(err)
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
